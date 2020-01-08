07ac66eae9c4768651190d1afc1450b510cae0b5
Manuel37896 <58004221+Manuel37896@users.noreply.github.com>
2020-01-08 12:13:34 -0600

    Initial commit

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public CheckpointManger chMgr;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Ball") 
+        {
+            chMgr.SetNewCheckpoint(transform.position);
+            Debug.Log("Checkpoint reached!");
+        }
+    }
+}
